50e52e1ba79f75c6d3c44e2b3b1325675554cba4
Wei FeiYang <wesley.f.young@outlook.com>
2025-12-17 16:32:46 +0800

    fix(core/js): migrate to tsdown; hack-fix `eval('require') pattern`

---
+++ packages/core/tsdown.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: ['../../acidify-core/build/dist/js/productionLibrary/acidify-acidify-core.mjs'],
+  format: 'esm',
+  clean: true,
+  dts: false,
+  target: false,
+  plugins: [
+    {
+      name: 'hack-eval-require',
+      transform(code, id) {
+        const banner =
+          `import { createRequire as __cr } from "node:module";\n` +
+          `const require = __cr(import.meta.url);\n`;
+        const hitRE = /eval\(\s*['"]require['"]\s*\)/
+        if (!/\.(mjs|js|mts|ts|jsx|tsx)$/.test(id)) return null;
+        if (!hitRE.test(code)) return null;
+        if (code.includes("createRequire as __cr") && code.includes("__cr(import.meta.url)")) {
+          return null;
+        }
+        return {
+          code: banner + code,
+          map: null,
+        };
+      }
+    }
+  ],
+});
--- packages/core/tsup.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['../../acidify-core/build/dist/js/productionLibrary/acidify-acidify-core.mjs'],
-  format: 'cjs',
-  clean: true,
-});
